5a3eb8e152fc1eb8adfc942b62b08990dbbdd73d
denztivoh <106525815+denztivoh@users.noreply.github.com>
2023-02-03 09:56:57 +0300

    Add files via upload

---
+++ operator 1.py
@@ -0,0 +1,7 @@
+#BANK LOAN
+age = int(input("Enter your age: "))
+income=int(input( "Enter your income: "))
+if (age>=21 and income>=21000):
+    print("Congratulations you qualify for loan")
+else:
+     print("Unfortunately, we are unable to offer you loan for this time")
